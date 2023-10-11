e8120af372efffd9c999449d7de33a51bae64ef5
Coding-Kshatriya <147599657+Coding-Kshatriya@users.noreply.github.com>
2023-10-11 14:35:38 +0530

    Create Namaste.c

---
+++ Examples/Namaste.c
@@ -0,0 +1,12 @@
+/************************
+  LCD_20x04 Library - Namaste
+
+ Demonstrates the use of a 20x4 LCD display. The LCD_20x04
+ the library works on a 20x04 LCD display that is compatible with the
+ Hitachi HD44780 driver. Many are out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This program prints "Namaste!" on the LCD
+ and shows the time.
+ ************************/
+
